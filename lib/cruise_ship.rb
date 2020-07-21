78569f751c61c2aaaaa233f580fd05f01fcc6aa0
Waverley Leung <waverley.nyc@gmail.com>
2020-07-21 17:57:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,4 +10,5 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   passengers.each do |suite, name|
-    if suite == suite_a && name.start_with?
+    if suite == suite_a && name.start_with?("A")
+      
