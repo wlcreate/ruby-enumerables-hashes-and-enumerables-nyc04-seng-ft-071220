b98a11a2901aca3ca7a43b2b6b330fd03e3dc87b
Waverley Leung <waverley.nyc@gmail.com>
2020-07-21 18:03:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,5 +7,5 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name} "
+    puts "Happy Birthday #{kids_name}! You are "
 end
