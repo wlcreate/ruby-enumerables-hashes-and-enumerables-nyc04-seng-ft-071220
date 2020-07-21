14ddae876a60f1adc976fc70fd5cfeed7bfb4082
Waverley Leung <waverley.nyc@gmail.com>
2020-07-21 17:55:43 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,9 +9,9 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  passengers.do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+  passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
   end
+end
 end
